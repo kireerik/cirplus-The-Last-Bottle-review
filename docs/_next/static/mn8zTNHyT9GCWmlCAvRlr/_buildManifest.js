9c3380e5049fd7910904877455464f885456aefc
kireerik <kireerike@gmail.com>
2023-03-01 05:50:44 +0100

    added basic production build

---
+++ docs/_next/static/mn8zTNHyT9GCWmlCAvRlr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/724-e488749c83fbabf0.js","static/chunks/pages/index-a719329ce7f37120.js"],"/_error":["static/chunks/pages/_error-e888bd823891e1c9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
